76911917cb6d56638d5858f58e90512b625dff8b
oblivm <oblivmteam@gmail.com>
2015-08-18 04:01:56 -0400

    Delete runtogether.sh
    this script should be generated automatically

--- runtogether.sh
+++
@@ -1,2 +0,0 @@
-java -cp to-run:lib/* com.oblivm.backend.lang.inter.Cmd -t gen -i $1  -c com.oblivm.backend.generated.hamming.NoClass &
-java -cp to-run:lib/* com.oblivm.backend.lang.inter.Cmd -t eva -i $2 -c com.oblivm.backend.generated.hamming.NoClass
