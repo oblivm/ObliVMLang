7a31a559d0ef87590527a94e20374610dcacd6d1
liuchangacm <liuchang2005acm@gmail.com>
2015-09-21 16:29:41 -0400

    allow assigning port

--- run-compiler.sh
+++ run-compiler.sh
@@ -1,5 +1,5 @@
 mkdir -p flexsc-bin;
 mkdir -p to-run;
-java -cp bin:lib/* com.oblivm.compiler.cmd.Cmd -o ./flexsc-bin/ $1;
+java -cp bin:lib/* com.oblivm.compiler.cmd.Cmd -o ./flexsc-bin/ $2;
 find flexsc-bin -name "*.java" > source.txt;
-javac -cp to-run:lib/* -d to-run @source.txt;
+javac -cp to-run:lib/* -d to-run -p $1 @source.txt;
